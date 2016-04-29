8dd81b8409c3f4c1bc5da184a1a34df75d6d42bc
yangyaochia <yangyaochiatw@gmail.com>
2016-04-29 23:15:58 +0800

    Added files via upload
    add in comment into the function

--- cachematrix.R
+++ cachematrix.R
@@ -6,6 +6,7 @@
 makeCacheMatrix <- function(x = matrix()) {
 
     i <- NULL
+    # four different methods for makeCacheMatrix object
     set <- function(y) {
         x <<- y
         i <<- NULL
@@ -23,6 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    # x is a makeCacheMatrix object, so it has getinverse method, so as others.
     i <- x$getinverse()
     if(!is.null(i)) {
         message("getting cached data")
